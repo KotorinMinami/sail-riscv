3cc341b44fc2f7f0cd5acc72bfb7bbf7b930c4bc
Mingzhu Yan <yanmingzhu@iscas.ac.cn>
2026-06-09 11:31:54 +0000

    Add ame ctest template

--- build_simulator.sh
+++ build_simulator.sh
@@ -2,7 +2,13 @@
 
 set -e
 : "${DOWNLOAD_GMP:=TRUE}"
-: "${ENABLE_RISCV_TESTS:=TRUE}"
-cmake -S . -B build -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDOWNLOAD_GMP="${DOWNLOAD_GMP}" -DENABLE_RISCV_TESTS="${ENABLE_RISCV_TESTS}"
+: "${ENABLE_RISCV_TESTS:=FALSE}"
+: "${ENABLE_RISCV_AME_TESTS:=TRUE}"
+cmake -S . -B build \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DDOWNLOAD_GMP="${DOWNLOAD_GMP}" \
+  -DENABLE_RISCV_TESTS="${ENABLE_RISCV_TESTS}" \
+  -DENABLE_RISCV_AME_TESTS="${ENABLE_RISCV_AME_TESTS}"
 jobs=$( (nproc || sysctl -n hw.ncpu || echo 2) 2>/dev/null)
 cmake --build build -j${jobs}
+ctest --test-dir build -R '^ame_' --output-on-failure
